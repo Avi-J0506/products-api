dce30ceae0ac1fae3c80a7042eb18552a30512ad
Abhinandan <jhaabhinandan55@gmail.com>
2024-04-13 03:26:59 +0530

    Controllers, Routes setup done

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+const products_routes = require('./routes/products');
+
 app.get('/', (req, res) => {
     res.send("API is running");
-})
+});
+
+app.use('/api/products', products_routes);
 
 const start = async()=>{
     try {
