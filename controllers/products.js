b2b39efb1332867b81cfa4f63e8c258e8ab16e9c
Abhinandan <jhaabhinandan55@gmail.com>
2024-04-13 05:22:44 +0530

    added feature for invalid queries

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,14 @@
 const Product = require("../models/productModel");
 
 const getAllProducts = async (req, res) => {
-    const myData = await Product.find(req.query);
+  const { company } = req.query;
+  const queryObject = {};
+
+  if(company) {
+    queryObject.company = company;
+    console.log(queryObject);
+  }
+  const myData = await Product.find(queryObject);
   res.status(200).json({ myData });
 };
 
--- productDB.js
+++ productDB.js
@@ -7,6 +7,7 @@ const ProductJson = require('./products.json');
 const start = async() => {
     try {
         await connectDB(process.env.MONGODB_URI)
+        await Product.deleteMany();
         await Product.create(ProductJson);
         console.log("Data added successfully");
     } catch (error) {
