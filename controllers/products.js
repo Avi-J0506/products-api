035c30b7f38492be281d478adc0d781f13deb4d0
Abhinandan <jhaabhinandan55@gmail.com>
2024-04-13 05:09:23 +0530

    filters added

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,13 @@
 const Product = require("../models/productModel");
 
 const getAllProducts = async (req, res) => {
-  const myData = await Product.find({company: "apple"});
+    const myData = await Product.find(req.query);
   res.status(200).json({ myData });
 };
 
 const getAllProductsTesting = async (req, res) => {
-  res.status(200).json({ msg: "I am getAllProductsTesting" });
+  const myData = await Product.find(req.query);
+  res.status(200).json({ myData });
 };
 
 module.exports = { getAllProducts, getAllProductsTesting };
